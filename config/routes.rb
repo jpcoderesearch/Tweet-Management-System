9a546bcb306bfaeb596a8c8545d2421737c81fc8
Arun P <arun.p@prontonetworks.net>
2020-05-26 14:15:31 +0530

    adding session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :v1 do
+    resources :sessions, only: [:create, :destroy]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
